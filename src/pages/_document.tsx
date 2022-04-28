0505557772892edeb2f7f716a819d153bd43402e
rmzNadir <dgzrz99@gmail.com>
2022-04-28 16:34:19 -0500

    build: :wrench: change config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,6 @@
 import { createGetInitialProps } from '@mantine/next';
-// @ts-expect-error
-import { abortableFetch } from 'abortcontroller-polyfill/dist/cjs-ponyfill';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import fetch from 'node-fetch';
 
-global.fetch = abortableFetch(fetch).fetch;
 const getInitialProps = createGetInitialProps();
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
